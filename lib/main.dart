895f9b07ffab4c3e1f1963689e314290dea85ceb
GuillermoHerrera <guillhermoherrera@gmail.com>
2024-01-09 17:53:59 -0600

    mask, captura & crop

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,147 @@
+import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+//import 'package:mask_for_camera_view/inside_line/mask_for_camera_view_inside_line.dart';
+import 'package:mask_for_camera_view/mask_for_camera_view.dart';
+import 'package:mask_for_camera_view/mask_for_camera_view_border_type.dart';
+import 'package:mask_for_camera_view/mask_for_camera_view_inside_line.dart';
+import 'package:mask_for_camera_view/mask_for_camera_view_inside_line_direction.dart';
+import 'package:mask_for_camera_view/mask_for_camera_view_inside_line_position.dart';
+import 'package:mask_for_camera_view/mask_for_camera_view_result.dart';
 
-void main() {
+//late List<CameraDescription> cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //cameras = await MaskForCameraView.initialize();
+  await MaskForCameraView.initialize();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        MaskForCameraView(
+          visiblePopButton: false,
+          appBarColor: Colors.blue,
+          title: 'Alinea la INE a la imagen y captura',
+          //bottomBarColor: Colors.red,
+          takeButtonActionColor: Colors.blue,
+          iconsColor: Colors.grey,
+          titleStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0),
+          borderType: MaskForCameraViewBorderType.dotted,
+          boxBorderWidth: 0,
+          boxBorderRadius: 0,
+          boxWidth: size.height * .3,
+          boxHeight: size.width * 1,
+          insideLine: MaskForCameraViewInsideLine(
+            position: MaskForCameraViewInsideLinePosition.partFour,
+            direction: MaskForCameraViewInsideLineDirection.vertical,
+          ),
+          // insideLine: MaskForCameraViewInsideLine(
+          //   position: MaskForCameraViewInsideLinePosition.endPartThree,
+          //   direction: MaskForCameraViewInsideLineDirection.horizontal,
+          // ),
+          // [cameras.first] is rear camera.
+          //cameraDescription: cameras.first,
+          onTake: (MaskForCameraViewResult? res) {
+            if (res != null) {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                backgroundColor: Colors.transparent,
+                builder: (context) => Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 14.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(26.0),
+                      topRight: Radius.circular(26.0),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "Cropped Images",
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 12.0),
+                      res.croppedImage != null
+                          ? MyImageView(imageBytes: res.croppedImage!)
+                          : Container(),
+                      const SizedBox(height: 8.0),
+                      Row(
+                        children: [
+                          res.firstPartImage != null
+                              ? Expanded(
+                                  child: MyImageView(
+                                      imageBytes: res.firstPartImage!))
+                              : Container(),
+                          res.firstPartImage != null &&
+                                  res.secondPartImage != null
+                              ? const SizedBox(width: 8.0)
+                              : Container(),
+                          res.secondPartImage != null
+                              ? Expanded(
+                                  child: MyImageView(
+                                      imageBytes: res.secondPartImage!))
+                              : Container(),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          },
+        ),
+        Center(
+          child: RotatedBox(
+            quarterTurns: 1,
+            child: Image(image: const AssetImage('assets/id_card_ine.png'), color: Colors.white.withOpacity(0.3), width: size.width * 1, height: size.height * 0.3, fit: BoxFit.fill)),
+        )
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyImageView extends StatelessWidget {
+  const MyImageView({Key? key, required this.imageBytes}) : super(key: key);
+  final Uint8List imageBytes;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4.0),
+      child: SizedBox(
+        width: double.infinity,
+        height: 200,
+        child: Image.memory(imageBytes),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
